f385b867ce8fd0bdb8a35d444b33f3b8559a68f1
Chih-Hui Wang <chihhui.x@gmail.com>
2023-09-16 23:54:15 +0800

    add wrong path log

--- app.js
+++ app.js
@@ -1,42 +1,49 @@
-const express = require('express');
-const session = require('express-session');
-const path = require('path');
-const dotenv = require('dotenv');
+const express = require("express");
+const session = require("express-session");
+const path = require("path");
+const dotenv = require("dotenv");
 
 const app = express();
 const port = 8000;
 
 dotenv.config();
 
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 app.set("views", path.join(__dirname, "./server/views"));
-app.set('view engine', 'pug');
+app.set("view engine", "pug");
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use(session({
-  name: 'member',
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: { SameSite: 'lax', maxAge: 5 * 60 * 1000 }
-}));
+app.use(
+	session({
+		name: "member",
+		secret: process.env.SESSION_SECRET,
+		resave: false,
+		saveUninitialized: false,
+		cookie: { SameSite: "lax", maxAge: 5 * 60 * 1000 },
+	})
+);
 
-const loginRoute = require('./server/routes/login');
+const loginRoute = require("./server/routes/login");
 app.use(loginRoute);
 
 app.get("/check", (req, res) => {
-    return res.send("Hello World!");
-})
+	return res.send("Hello World!");
+});
 
 // --------------------------------------------------------
+app.use((req, res, next) => {
+	console.error("ERROR req.path: ", req.path);
+	res.status(404).send("Page Not Found!");
+});
+
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broken!');
+	console.error(err.stack);
+	res.status(500).send("Something Broken!");
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}...`);
+	console.log(`Server is running on port ${port}...`);
 });
